ef4630c6dcc4ece16366b576e70cf26c9a5866f2
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-11-12 20:37:30 -0500

    my changes

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Background from "./components/Background/Background";
 import Nav from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/home" component={Home} />
+          <Route exact path="/login" component={Login} />
         </Switch>
       </div>
     </Router>
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
+// const routes = require("./routes");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3001;
