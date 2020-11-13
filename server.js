5ff6b769693cafbe589b8ab8bf65b324bc3df1cc
Haynes Brotherton <hbrotherton44@gmail.com>
2020-11-12 19:15:13 -0500

    added path to server.js for path not defined error

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 // const routes = require("./routes");
+const path = require('path')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
