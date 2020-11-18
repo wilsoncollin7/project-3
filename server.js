93c157622a701131f47f8dc704c86bbd6bf4de68
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-11-17 19:45:55 -0500

    front end api calls for saved and visited trails

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-// const routes = require("./routes");
+// const routes = require("./server/routes");
 const app = express();
 // const path = require("path");
 const PORT = process.env.PORT || 3001;
@@ -9,17 +9,14 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 // app.use(routes);
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-// Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
 app.listen(PORT, function() {
