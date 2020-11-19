6dc28b3917b4a292e1023bd1841830dc99d1bc1d
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-11-19 18:15:06 -0500

    got the db connection to work

--- server/models/userSeed.js
+++ server/models/userSeed.js
@@ -1,39 +1,39 @@
-const mongoose = require('mongoose');
-mongoose.promise = Promise;
+// const mongoose = require('mongoose');
+// mongoose.promise = Promise;
 
-let userSeed = [
-  {
-    firstName: {
-      type: String,
-      required: true
-      },
-    lastName: {
-      type: String,
-      required:true
-      },
-    email: {
-      type: String,
-      required: true
-      },
-    password: {
-      type: String,
-      required: true, 
-      minlength: 8
-      }
-  }
-];
+// let userSeed = [
+//   {
+//     firstName: {
+//       type: String,
+//       required: true
+//       },
+//     lastName: {
+//       type: String,
+//       required:true
+//       },
+//     email: {
+//       type: String,
+//       required: true
+//       },
+//     password: {
+//       type: String,
+//       required: true, 
+//       minlength: 8
+//       }
+//   }
+// ];
 
-db.UserInfo.deleteMany({})
-  .then(() => db.UserInfo.collection.insertMany(userSeed))
-  .then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+// db.UserInfo.deleteMany({})
+//   .then(() => db.UserInfo.collection.insertMany(userSeed))
+//   .then(data => {
+//     console.log(data.result.n + " records inserted!");
+//     process.exit(0);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//     process.exit(1);
+//   });
 
-const User = mongoose.model('User', userSeed);
+// const User = mongoose.model('User', userSeed);
 
-module.exports = User;
+// module.exports = User;
