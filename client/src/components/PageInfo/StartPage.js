7919a45934266841586e96a647746508bc305a96
Ivy Bean <ivieking@gmail.com>
2020-11-19 21:07:24 -0500

    remove doubled searchBtn

--- client/src/components/PageInfo/StartPage.js
+++ client/src/components/PageInfo/StartPage.js
@@ -54,8 +54,7 @@ class StartPage extends Component {
     return (
       <div>
       <form className="searchBar">
-        <input onChange={this.handleOnChange} style= {{width: "300px", background:"#F2F1F9", border:"none", padding: "1%"}} type="text" placeholder="Where we going?" value={this.state.search}></input>
-        <button className="btn btn-success" onClick={this.handleSubmitSearch}>Go Out Yonder!</button>
+        <input onChange={this.handleOnChange} style= {{width: "300px", background:"#F2F1F9", border:"none", padding: "1%", marginRight:"2%"}} type="text" placeholder="Where we going?" value={this.state.search}></input>
         <StateDropdown className="searchbar" />
         <button className="btn" id="searchBtn" style= {{border:"none", width: "20%", background: "#144552ac", marginLeft:"2%"}} onClick={this.handleSubmitSearch}>Go Out Yonder!</button>
         
