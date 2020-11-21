04077e9dd025ba06bf4feea49377b1b129ba09be
Garrett Dobson <garrettdobson33@gmail.com>
2020-11-21 13:19:23 -0500

    updated file paths so nodemon funcitons properly for auth

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const authRoutes = require('.auth');
+const authRoutes = require('./auth');
 
 // API Routes
 router.use('/api', apiRoutes);
