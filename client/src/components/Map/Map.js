ff96d6d47be17fbc77bc575b4ccb9bf0709a06b2
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-11-21 13:43:08 -0500

    my changes

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import GoogleMapReact from 'google-map-react';
 import "./Map.css";
-import Image from "./MapImage"
 
-//this is the map that will display a trail they search
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
 function Map(props) {
+  // const [coord, setcoord] = useState({
+  //   center: {
+  //     lat: "",
+  //     lng: ""
+  //   },
+  //   zoom: 11
+  // })
+
+  let center = {
+    lat: 39.83076929,
+    lng: -75.56353942
+  }
+
+  console.log(props.lat)
+  console.log(props.lng)
+  
   return (
-    <div>
-      <div className="mapDescription">
-        <p><Image className="mapImage"></Image></p>
-        <p>{props.lat}</p>
-        <p>{props.lng}</p>
-      </div>
+    // Important! Always set the container height explicitly
+    <div style={{ height: '500px', width: '500px', margin: 'auto' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyDwjr75wpVbrqdqfwE_Gb41DcE3T8s04wM" }}
+        defaultCenter={center}
+        defaultZoom="11"
+      >
+        <AnyReactComponent
+          lat={center.lat}
+          lng={center.lng}
+          text="My Marker"
+        />
+      </GoogleMapReact>
     </div>
-  )
+  );
 }
 
 export default Map;
