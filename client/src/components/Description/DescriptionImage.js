6046ed18b16e9ea22eeb04fdadbb7d68550b78d8
Tanner Cook <tannercook336@gmail.com>
2020-12-01 18:30:40 -0500

    merge conflicts

--- client/src/components/Description/DescriptionImage.js
+++ client/src/components/Description/DescriptionImage.js
@@ -4,9 +4,6 @@ import "./Description.css"
 
 
 class Image extends Component {
-  constructor(props) {
-    super(props)
-  }
 
   render () {
     return <img src={this.props.image} className="descriptionImage" alt="ParkPic" />;
