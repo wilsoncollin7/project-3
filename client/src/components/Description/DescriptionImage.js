35773926ffdeade4c567c754155696452b6aeeec
Tanner Cook <tannercook336@gmail.com>
2020-12-01 21:08:29 -0500

    added styling for the description page

--- client/src/components/Description/DescriptionImage.js
+++ client/src/components/Description/DescriptionImage.js
@@ -4,13 +4,11 @@ import "./Description.css"
 
 
 class Image extends Component {
-<<<<<<< HEAD
-=======
   // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props)
+    console.log(this.props)
   }
->>>>>>> a7cd51aee56c37134eb5d018d031dc69aff2623d
 
   render () {
     return <img src={this.props.image} className="descriptionImage" alt="ParkPic" />;
