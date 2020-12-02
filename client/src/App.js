55a0a706baf339ea209136315b0e1422b990b32e
Ivy Bean <ivieking@gmail.com>
2020-12-01 21:09:18 -0500

    api call update

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Dashboard from "./pages/Dashboard";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+
 function App() {
   // setting user state to be passed as context 
   const [user, setUser] = useState({
